6a083046fef1968b62fa2122ca362ce82299ab4b
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-24 23:30:44 +0530

    added practice files

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read -s UN
+
+echo "HI ${UN}!"
+
+echo "Please enter the password"
+read -s PWD
+
+echo "this is your ${PWD}"
---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+######
+#### This is array######
+
+FRUITS=("apple" "banana" "kiwi" "guava" "papaya")
+
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Fruits are ${FRUITS[@]}"
