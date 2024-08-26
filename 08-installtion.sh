23f0da13ca5eafda537e84b4c214bfdafab68685
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-26 11:55:18 +0530

    committed

---
+++ 08-installtion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ ${USERID} -ne 0 ]
+then
+    echo "Please run the script with sudo previleges"
+    exit 1
+fi
+dnf list installed  mysql 
+
+if [ $? -ne 0 ]
+then
+    echo "my sql was not installed, going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "mysql was installed successfully"
+    fi
+else
+    echo "Installtion was failed"
+    exit 1
+fi
