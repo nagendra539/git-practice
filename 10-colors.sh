d6cd2f99f190738ac6786b3fe611ff951459dcd0
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-26 22:20:15 +0530

    committed

---
+++ 10-colors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+RED="\e[31m"
+GREEN="\e[32m"
+N="\e[0m"
+
+
+if [ ${USERID} -ne 0 ]
+then
+    echo -e "Please run the script with ${RED} sudo previleges ${N}"
+    exit 1
+fi
+
+VERIFY()
+{
+    if [ ${1} -ne 0 ]
+    then 
+        echo "installtion failed..."
+        exit 1
+    else    
+        echo -e "installation of ${GREEN} ${2} passed ${N}"
+    fi
+}
+
+dnf list installed  mysql 
+
+if [ $? -ne 0 ]
+then
+    echo "my sql was not installed, going to install it"
+    dnf install mysql -y
+    VERIFY $? mysql
+    
+else
+    echo "package was already installed"
+    exit 1
+fi
