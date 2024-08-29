d6d1d40976aad397b7fac2c111da8bb28505027d
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-29 17:11:00 +0530

    commited

--- 15-threold.sh
+++ 15-threold.sh
@@ -5,7 +5,7 @@ USAGE=5
 
 while IFS= read -r line
 do
-    PARTITION=$(echo "$line" | awk -F " " '{print $1F}')
+    PARTITION=$(echo "$line" | awk -F " " '{print $1F}'| cut -d "%" -f1)
     UTILIZATION=$(echo "$line" | awk -F " " '{print $6F}')
     if [ $UTILIZATION -ge $USAGE ]
     then
