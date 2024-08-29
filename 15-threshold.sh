b51a29f29e0a28a8f08d5e9a8f82d9c3a1dfe56b
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-29 19:52:14 +0530

    commited

--- 15-threshold.sh
+++ 15-threshold.sh
@@ -5,7 +5,7 @@ USAGE=5
 
 while IFS= read -r line
 do
-    PARTITION=$(echo "$line" | awk -F " " '{print $1F}')
+    PARTITION=$(echo "$line" | awk -F " " '{print $NF}') #$NF: get the last column
     UTILIZATION=$(echo "$line" | awk -F " " '{print $6F}' | cut -d "%" -f1)
     if [ $UTILIZATION -ge $USAGE ]
     then
