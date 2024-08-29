c79a9ef6195e32f9c5e9ee601ba41e984ef07b7d
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-29 16:26:03 +0530

    commited

--- 15-threold.sh
+++ 15-threold.sh
@@ -4,6 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 
 while IFS= read -r line
 do
-    LINE1=echo "$line" | awk -F " " '{print $1F}'
-    LINE2=echo "$line" | awk -F " " '{print $6F}'
+    LINE1=$(echo "$line" | awk -F " " '{print $1F}')
+    LINE2=$(echo "$line" | awk -F " " '{print $6F}')
+    echo "$LINE1 : $LINE2"
 done <<< $DISK_USAGE
