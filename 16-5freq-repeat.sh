ad2aaa2f3c41433d990ae2b6f0f604769d40ddbc
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-31 09:40:20 +0530

    this is

--- 16-5freq-repeat.sh
+++ 16-5freq-repeat.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-FILE=$1
 if [ $# -lt 1 ]
 then
     echo "Please enter the filename as input"
     exit 1
 fi
+FILE=$1
+
 if [ -f $FILE ]
 then    
     echo "File exists"
@@ -17,4 +18,4 @@ fi
 while IFS= read -r line
 do  
     echo "$line"
-done <<< "$FILE"
+done <<< ${FILE}
